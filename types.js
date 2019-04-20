72e57d67753734ef220a4abb253428a4f1547885
Caleb Spain <crspain@uh.edu>
2019-04-20 13:15:52 -0500

    Learning TypeScript

---
+++ types.js
@@ -0,0 +1,25 @@
+var myString;
+var myNum;
+var myBool;
+var myVar;
+/*
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+*/
+var strArr;
+var numArr;
+var boolArr;
+var strNumTuple;
+myString = 'Hello World'.slice(0, 3);
+myNum = 22;
+myBool = true;
+myVar = 5;
+strArr = ['a', 'b', 'c'];
+numArr = [1, 2, 3];
+boolArr = [true, false, true];
+strNumTuple = ['Hello', 2];
+//var myVoid = undefined;
+var mynull = null;
+var myUndefined = null;
+console.log(myVoid);
---
+++ types.ts
@@ -0,0 +1,33 @@
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+/*
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+*/
+
+let strArr: Array<string>;
+let numArr: Array<number>;
+let boolArr: Array<boolean>;
+
+let strNumTuple: [string, number];
+
+myString = 'Hello World'.slice(0, 3);
+myNum = 22;
+myBool = true;
+myVar = 5; 
+
+strArr = ['a', 'b', 'c'];
+numArr = [1, 2, 3];
+boolArr = [true, false, true];
+
+strNumTuple = ['Hello', 2];
+
+//let myVoid: void = undefined;
+let mynull: null = null;
+let myUndefined: undefined = null;
+
+console.log(myVoid);
